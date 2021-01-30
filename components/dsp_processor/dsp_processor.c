a1f65dfb566b40472bc12d1ff73e3943980dbdcf
David Douard <david.douard@sdfa3.org>
2021-01-30 14:04:58 +0100

    Replace usage of ll dsps_biquad_f32_ae32 by dsps_biquad_f32

--- components/dsp_processor/dsp_processor.c
+++ components/dsp_processor/dsp_processor.c
@@ -13,19 +13,20 @@
 #include "dsp_processor.h"
 
 
-uint32_t bits_per_sample = CONFIG_BITS_PER_SAMPLE; 
+uint32_t bits_per_sample = CONFIG_BITS_PER_SAMPLE;
 
 static xTaskHandle s_dsp_i2s_task_handle = NULL;
 static RingbufHandle_t s_ringbuf_i2s = NULL;
 
 extern xQueueHandle i2s_queue;
 
-extern uint32_t buffer_ms;  
+extern uint32_t buffer_ms;
 extern uint8_t muteCH[4];
 
 uint dspFlow = dspfStereo;
 
 ptype_t bq[6];
+
 
 void setup_dsp_i2s(uint32_t sample_rate, bool slave_i2s)
 {
@@ -35,8 +36,8 @@ void setup_dsp_i2s(uint32_t sample_rate, bool slave_i2s)
     .bits_per_sample = bits_per_sample,
     .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,                           // 2-channels
     .communication_format = I2S_COMM_FORMAT_I2S ,
-    .dma_buf_count = 8, 
-    .dma_buf_len = 480, 
+    .dma_buf_count = 8,
+    .dma_buf_len = 480,
     .intr_alloc_flags = 1,                                                  //Default interrupt priority
     .use_apll = true,
     .fixed_mclk = 0,
@@ -49,19 +50,19 @@ void setup_dsp_i2s(uint32_t sample_rate, bool slave_i2s)
     .data_out_num = CONFIG_MASTER_I2S_DATAOUT_PIN,
     .data_in_num  = -1                                                       //Not used
   };
-  
+
   i2s_driver_install(0, &i2s_config0, 7, &i2s_queue);
   i2s_zero_dma_buffer(0);
   i2s_set_pin(0, &pin_config0);
 
   i2s_config_t i2s_config1 = {
-    .mode = I2S_MODE_SLAVE | I2S_MODE_TX,                                   // Only TX - Slave channel 
+    .mode = I2S_MODE_SLAVE | I2S_MODE_TX,                                   // Only TX - Slave channel
     .sample_rate = sample_rate,
     .bits_per_sample = bits_per_sample,
     .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,                           // 2-channels
     .communication_format = I2S_COMM_FORMAT_I2S,
-    .dma_buf_count = 8, 
-    .dma_buf_len = 480, 
+    .dma_buf_count = 8,
+    .dma_buf_len = 480,
     .use_apll = true,
     .fixed_mclk = 0,
     .tx_desc_auto_clear = true                                              // Auto clear tx descriptor on underflow
@@ -73,7 +74,7 @@ void setup_dsp_i2s(uint32_t sample_rate, bool slave_i2s)
     .data_out_num =  CONFIG_SLAVE_I2S_DATAOUT_PIN,
     .data_in_num = -1                                                       //Not used
   };
-  
+
   if (slave_i2s) {
     i2s_driver_install(1, &i2s_config1, 7, &i2s_queue);
     i2s_zero_dma_buffer(1);
@@ -103,54 +104,54 @@ static void dsp_i2s_task_handler(void *arg)
   muteCH[2] = 0;
   muteCH[3] = 0;
   uint32_t inBuffer,freeBuffer,wbuf,rbuf ;
-  
+
   static int32_t avgcnt = 0;
-  uint32_t avgcntlen = 64;  // x 960/4*1/fs = 320ms @48000 kHz     
+  uint32_t avgcntlen = 64;  // x 960/4*1/fs = 320ms @48000 kHz
   uint32_t avgarray[128] = {0};
-  uint32_t sum; 
-  float avg ; 
+  uint32_t sum;
+  float avg ;
   for (;;) {
-    // Condition                                     state   Action    
-    // Buffer is empty - because not being filled    Stopped  Wait 
-    // Buffer is increasing and below target         Filling  Wait 
-    // Buffer above target                           Playing  Consume from buffer     
+    // Condition                                     state   Action
+    // Buffer is empty - because not being filled    Stopped  Wait
+    // Buffer is increasing and below target         Filling  Wait
+    // Buffer above target                           Playing  Consume from buffer
     // Buffer is below target                        Playing  Short delay
-     
+
     cnt++;
-    audio = (uint8_t *)xRingbufferReceiveUpTo(s_ringbuf_i2s, &chunk_size,(portTickType) 20 ,960);  // 200 ms timeout 
-    
-    vRingbufferGetInfo(s_ringbuf_i2s, &freeBuffer, &rbuf, &wbuf, NULL, &inBuffer ); 
-    
+    audio = (uint8_t *)xRingbufferReceiveUpTo(s_ringbuf_i2s, &chunk_size,(portTickType) 20 ,960);  // 200 ms timeout
+
+    vRingbufferGetInfo(s_ringbuf_i2s, &freeBuffer, &rbuf, &wbuf, NULL, &inBuffer );
+
     if (avgcnt >= avgcntlen) { avgcnt = 0; }
-    avgarray[avgcnt++] = inBuffer; 
+    avgarray[avgcnt++] = inBuffer;
     sum = 0;
-    for (int n = 0; n < avgcntlen ; n++) 
-    { sum = sum + avgarray[n];     
+    for (int n = 0; n < avgcntlen ; n++)
+    { sum = sum + avgarray[n];
     }
-    avg = sum / avgcntlen;    
-    
-    #ifndef CONFIG_USE_PSRAM 
+    avg = sum / avgcntlen;
+
+    #ifndef CONFIG_USE_PSRAM
       buffer_ms = 150;
-    #endif 
-    
-    if (inBuffer < (buffer_ms*48*4)) {vTaskDelay(1); }  
-    
-    //audio = (uint8_t *)xRingbufferReceiveUpTo(s_ringbuf_i2s, &chunk_size,(portTickType) 20 ,960);  // 200 ms timeout 
-    
+    #endif
+
+    if (inBuffer < (buffer_ms*48*4)) {vTaskDelay(1); }
+
+    //audio = (uint8_t *)xRingbufferReceiveUpTo(s_ringbuf_i2s, &chunk_size,(portTickType) 20 ,960);  // 200 ms timeout
+
     //audio = (uint8_t *)xRingbufferReceive(s_ringbuf_i2s, &chunk_size, (portTickType)portMAX_DELAY);
-    if (chunk_size == 0) 
+    if (chunk_size == 0)
     { printf("wait ... buffer : %d\n",inBuffer);
-    } 
-    //else if (inBuffer < (buffer_ms*48*4)) 
-    //{ printf("Buffering ...  buffer : %d\n",inBuffer); 
+    }
+    //else if (inBuffer < (buffer_ms*48*4))
+    //{ printf("Buffering ...  buffer : %d\n",inBuffer);
     //}
-    else 
+    else
     {   int16_t len = chunk_size/4;
         if (cnt%200 < 16)
         { ESP_LOGI("I2S", "Chunk :%d %d %.0f",chunk_size, inBuffer, avg );
           //xRingbufferPrintInfo(s_ringbuf_i2s);
         }
-        
+
         /*for (uint16_t i=0;i<len;i++)
         {
           sbuffer0[i] = ((float) ((int16_t) (audio[i*4+1]<<8) + audio[i*4+0]))/32768;
@@ -171,21 +172,21 @@ static void dsp_i2s_task_handler(void *arg)
                 audio[i*4+2] = (muteCH[1] == 1)? 0 : audio[i*4+2];
                 audio[i*4+3] = (muteCH[1] == 1)? 0 : audio[i*4+3];
               }
-              if (bits_per_sample == 16) { 
-                i2s_write(0,(char*)audio,  chunk_size, &bytes_written, portMAX_DELAY); 
-              } else 
+              if (bits_per_sample == 16) {
+                i2s_write(0,(char*)audio,  chunk_size, &bytes_written, portMAX_DELAY);
+              } else
               { i2s_write_expand(0, (char*)audio, chunk_size,16,32, &bytes_written, portMAX_DELAY);
               }
             }
             break;
           case dspfBiamp :
             { // Process audio ch0 LOW PASS FILTER
-              dsps_biquad_f32_ae32(sbuffer0, sbuftmp0, len, bq[0].coeffs, bq[0].w);
-              dsps_biquad_f32_ae32(sbuftmp0, sbufout0, len, bq[1].coeffs, bq[1].w);
+              dsps_biquad_f32(sbuffer0, sbuftmp0, len, bq[0].coeffs, bq[0].w);
+              dsps_biquad_f32(sbuftmp0, sbufout0, len, bq[1].coeffs, bq[1].w);
 
               // Process audio ch1 HIGH PASS FILTER
-              dsps_biquad_f32_ae32(sbuffer0, sbuftmp0, len, bq[2].coeffs, bq[2].w);
-              dsps_biquad_f32_ae32(sbuftmp0, sbufout1, len, bq[3].coeffs, bq[3].w);
+              dsps_biquad_f32(sbuffer0, sbuftmp0, len, bq[2].coeffs, bq[2].w);
+              dsps_biquad_f32(sbuftmp0, sbufout1, len, bq[3].coeffs, bq[3].w);
 
               int16_t valint[2];
               for (uint16_t i=0; i<len; i++)
@@ -196,9 +197,9 @@ static void dsp_i2s_task_handler(void *arg)
                 dsp_audio[i*4+2] = (valint[1] & 0xff);
                 dsp_audio[i*4+3] = ((valint[1] & 0xff00)>>8);
               }
-              if (bits_per_sample == 16) { 
-                i2s_write(0,(char*)audio,  chunk_size, &bytes_written, portMAX_DELAY); 
-              } else 
+              if (bits_per_sample == 16) {
+                i2s_write(0,(char*)audio,  chunk_size, &bytes_written, portMAX_DELAY);
+              } else
               { i2s_write_expand(0, (char*)audio, chunk_size,16,32, &bytes_written, portMAX_DELAY);
               }
             }
@@ -206,16 +207,16 @@ static void dsp_i2s_task_handler(void *arg)
 
           case dspf2DOT1 :
             { // Process audio L + R LOW PASS FILTER
-              dsps_biquad_f32_ae32(sbuffer2, sbuftmp0, len, bq[0].coeffs, bq[0].w);
-              dsps_biquad_f32_ae32(sbuftmp0, sbufout2, len, bq[1].coeffs, bq[1].w);
+              dsps_biquad_f32(sbuffer2, sbuftmp0, len, bq[0].coeffs, bq[0].w);
+              dsps_biquad_f32(sbuftmp0, sbufout2, len, bq[1].coeffs, bq[1].w);
 
               // Process audio L HIGH PASS FILTER
-              dsps_biquad_f32_ae32(sbuffer0, sbuftmp0, len, bq[2].coeffs, bq[2].w);
-              dsps_biquad_f32_ae32(sbuftmp0, sbufout0, len, bq[3].coeffs, bq[3].w);
+              dsps_biquad_f32(sbuffer0, sbuftmp0, len, bq[2].coeffs, bq[2].w);
+              dsps_biquad_f32(sbuftmp0, sbufout0, len, bq[3].coeffs, bq[3].w);
 
               // Process audio R HIGH PASS FILTER
-              dsps_biquad_f32_ae32(sbuffer1, sbuftmp0, len, bq[4].coeffs, bq[4].w);
-              dsps_biquad_f32_ae32(sbuftmp0, sbufout1, len, bq[5].coeffs, bq[5].w);
+              dsps_biquad_f32(sbuffer1, sbuftmp0, len, bq[4].coeffs, bq[4].w);
+              dsps_biquad_f32(sbuftmp0, sbufout1, len, bq[5].coeffs, bq[5].w);
 
               int16_t valint[5];
               for (uint16_t i=0; i<len; i++)
@@ -238,16 +239,16 @@ static void dsp_i2s_task_handler(void *arg)
             break;
           case dspfFunkyHonda :
             { // Process audio L + R LOW PASS FILTER
-              dsps_biquad_f32_ae32(sbuffer2, sbuftmp0, len, bq[0].coeffs, bq[0].w);
-              dsps_biquad_f32_ae32(sbuftmp0, sbufout2, len, bq[1].coeffs, bq[1].w);
+              dsps_biquad_f32(sbuffer2, sbuftmp0, len, bq[0].coeffs, bq[0].w);
+              dsps_biquad_f32(sbuftmp0, sbufout2, len, bq[1].coeffs, bq[1].w);
 
               // Process audio L HIGH PASS FILTER
-              dsps_biquad_f32_ae32(sbuffer0, sbuftmp0, len, bq[2].coeffs, bq[2].w);
-              dsps_biquad_f32_ae32(sbuftmp0, sbufout0, len, bq[3].coeffs, bq[3].w);
+              dsps_biquad_f32(sbuffer0, sbuftmp0, len, bq[2].coeffs, bq[2].w);
+              dsps_biquad_f32(sbuftmp0, sbufout0, len, bq[3].coeffs, bq[3].w);
 
               // Process audio R HIGH PASS FILTER
-              dsps_biquad_f32_ae32(sbuffer1, sbuftmp0, len, bq[4].coeffs, bq[4].w);
-              dsps_biquad_f32_ae32(sbuftmp0, sbufout1, len, bq[5].coeffs, bq[5].w);
+              dsps_biquad_f32(sbuffer1, sbuftmp0, len, bq[4].coeffs, bq[4].w);
+              dsps_biquad_f32(sbuftmp0, sbufout1, len, bq[5].coeffs, bq[5].w);
 
               uint16_t scale = 16384;  //32768
               int16_t valint[5];
@@ -285,9 +286,9 @@ static void dsp_i2s_task_handler(void *arg)
     }
   }
 }
-// buffer size must hold 400ms-1000ms  // for 2ch16b48000 that is 76800 - 192000 or 75-188 x 1024    
+// buffer size must hold 400ms-1000ms  // for 2ch16b48000 that is 76800 - 192000 or 75-188 x 1024
 
-#define BUFFER_SIZE  192*1024  
+#define BUFFER_SIZE  192*1024
 
 void dsp_i2s_task_init(uint32_t sample_rate,bool slave)
 { setup_dsp_i2s(sample_rate,slave);
@@ -299,11 +300,11 @@ void dsp_i2s_task_init(uint32_t sample_rate,bool slave)
    uint8_t *buffer_storage = (uint8_t *)heap_caps_malloc(sizeof(uint8_t)*BUFFER_SIZE, MALLOC_CAP_SPIRAM);
    printf("Buffer_stoarge ok\n");
    s_ringbuf_i2s = xRingbufferCreateStatic(BUFFER_SIZE, RINGBUF_TYPE_BYTEBUF, buffer_storage, buffer_struct);
-   printf("Ringbuf ok\n"); 
-  #else 
+   printf("Ringbuf ok\n");
+  #else
    printf("Setup ringbuffer using internal ram - only space for 150ms - Snapcast buffer_ms parameter ignored \n");
-   s_ringbuf_i2s = xRingbufferCreate(32*1024,RINGBUF_TYPE_BYTEBUF); 
-  #endif 
+   s_ringbuf_i2s = xRingbufferCreate(32*1024,RINGBUF_TYPE_BYTEBUF);
+  #endif
   if (s_ringbuf_i2s == NULL) { printf("nospace for ringbuffer\n"); return; }
   printf("Ringbuffer ok\n");
   xTaskCreate(dsp_i2s_task_handler, "DSP_I2S", 48*1024, NULL, 6, &s_dsp_i2s_task_handle);
