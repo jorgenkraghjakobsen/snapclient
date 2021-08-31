3d141ea689b8be9b0926a716e063a89bee39519c
jorgenkraghjakobsen <jkj@myrun.dk>
2021-08-31 02:23:00 +0200

    change lic code

--- main/main.c
+++ main/main.c
@@ -3,7 +3,9 @@
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
+  Change licens code 
 */
+
 
 #include <string.h>
 
